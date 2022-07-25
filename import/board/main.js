62a1c7eb0befa588d0b22a1f72527013b471340b
Hiusu <luis_vala_junior@hotmail.com>
2022-07-25 13:38:42 -0300

    included function placePiece and style of board

--- import/board/main.js
+++ import/board/main.js
@@ -1,6 +1,8 @@
-function createTileButton() {
+function createTileButton(id, color) {
     let button_tile = document.createElement("button");
-    button_tile.innerHTML = "Testex";
+    button_tile.setAttribute("id", id)
+    button_tile.setAttribute("class", color)
+    button_tile.innerHTML = '<img src = "static/img/empty.png">';
     return button_tile;
 }
 
@@ -9,14 +11,27 @@ function initiateTable(board, row_amount, col_amount) {
         let board_row = document.createElement("tr");
         for(let c = 0; c < col_amount; c++) {
             let row_data = document.createElement("td");
-            row_data.appendChild(createTileButton());
+            sum = r*3 + c;
+            if(sum%2) {
+                row_data.appendChild(createTileButton(sum, "black"));
+            }
+            else {
+                row_data.appendChild(createTileButton(sum, "white"));
+            }
             board_row.appendChild(row_data);
         }
         board.appendChild(board_row);
     }
 }
 
+function placePiece(piece, team, tile) {
+    let button_tile = document.getElementById(tile)
+    button_tile.innerHTML = `<img src = "static/img/${piece}_${team}.png">`;
+}
+
 documentReady(()=>{
     let board = document.getElementById("boardTable");
-    initiateTable(board, 3, 3);
+    initiateTable(board, 8, 8);
+    placePiece("bishop", "black", 2);
+    placePiece("queen", "black", 3);
 });
