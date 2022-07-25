dafc59b50fbe46cbb9fa7a17c624a3c182cfe67b
Hiusu <luis_vala_junior@hotmail.com>
2022-07-25 14:08:06 -0300

    fixed issues with pairs and odds boards

--- import/board/main.js
+++ import/board/main.js
@@ -11,13 +11,30 @@ function initiateTable(board, row_amount, col_amount) {
         let board_row = document.createElement("tr");
         for(let c = 0; c < col_amount; c++) {
             let row_data = document.createElement("td");
-            sum = r*3 + c;
-            if(sum%2) {
+            sum = r*row_amount + c;
+            if(row_amount%2) {
+                if(sum%2) {
+                    row_data.appendChild(createTileButton(sum, "black"));
+                }
+                else {
+                    row_data.appendChild(createTileButton(sum, "white"));
+                }
+            }
+            else if(r%2) {
+                if(sum%2) {
+                    row_data.appendChild(createTileButton(sum, "white"));
+                }
+                else {
+                    row_data.appendChild(createTileButton(sum, "black"));
+                }
+            }
+            else if(sum%2){
                 row_data.appendChild(createTileButton(sum, "black"));
             }
             else {
-                row_data.appendChild(createTileButton(sum, "white"));
+                row_data.appendChild(createTileButton(sum, "white"));                
             }
+            
             board_row.appendChild(row_data);
         }
         board.appendChild(board_row);
@@ -31,7 +48,7 @@ function placePiece(piece, team, tile) {
 
 documentReady(()=>{
     let board = document.getElementById("boardTable");
-    initiateTable(board, 8, 8);
+    initiateTable(board, 3, 3);
     placePiece("bishop", "black", 2);
     placePiece("queen", "black", 3);
 });
