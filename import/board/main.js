8a622287f699d7bfaacebe94b0fe3b4ec7b2c700
Hiusu <luis_vala_junior@hotmail.com>
2022-07-29 15:29:57 -0300

    make the pieces moves and initiate the board menu

--- import/board/main.js
+++ import/board/main.js
@@ -1,3 +1,6 @@
+let rows = 0;
+let columns = 0;
+
 class Board {
     board_js = [];
 
@@ -20,28 +23,51 @@ class Board {
     }
     
     deletePossibleMoves() {
-        let clicked_tiles = document.getElementsByClassName("clicked");
-        while(clicked_tiles.length) {
-            clicked_tiles[0].style.borderColor = "transparent";
-            clicked_tiles[0].classList.remove("clicked");
+        let showed_tiles = document.getElementsByClassName("showed");
+        while(showed_tiles.length) {
+            showed_tiles[0].style.borderColor = "transparent";
+            showed_tiles[0].classList.remove("showed", "clicked");
         }
     }
 
     showPossibleMoves(id) {
-        this.deletePossibleMoves();
-
         let button_tile = document.getElementById(id);
+        let showed_tiles =  document.getElementsByClassName("showed");
         let position = this.idToPosition(id);
+        let flag = false
+        for(let i in showed_tiles) {
+            if(showed_tiles[i] == button_tile) {
+                flag = true;
+            }
+        }
 
-        if(this.board_js[position[0]][position[1]] != ChessPiece.EMPTY_TILE) {
-            button_tile.style.borderColor = "black";
-            button_tile.classList.add("clicked");
-            let possible_moves = this.board_js[position[0]][position[1]].getPossibleMoves([position[0], position[1]], this.board_js);
-            for(let m in possible_moves) {
-                let tile = this.positionToId(possible_moves[m]);
-                button_tile = document.getElementById(tile);
+        if(flag) {
+            let clicked_button = document.getElementsByClassName("clicked");
+            if(button_tile == clicked_button[0]) {
+                this.deletePossibleMoves();
+            }
+            else {
+                let nodeMap = document.getElementsByClassName("clicked").item(0).attributes;
+                let origin_id = nodeMap[0].value;
+                let origin_position = this.idToPosition(origin_id);
+                let piece = this.board_js[origin_position[0]][origin_position[1]];
+                this.placePiece(piece, position);
+            }
+        }
+        else {
+            this.deletePossibleMoves();
+            let piece = this.board_js[position[0]][position[1]];
+
+            if(piece != ChessPiece.EMPTY_TILE) {
                 button_tile.style.borderColor = "black";
-                button_tile.classList.add("clicked");
+                button_tile.classList.add("clicked", "showed");
+                let possible_moves = piece.getPossibleMoves([position[0], position[1]], this.board_js);
+                for(let m in possible_moves) {
+                    let tile = this.positionToId(possible_moves[m]);
+                    button_tile = document.getElementById(tile);
+                    button_tile.style.borderColor = "#4e4e4e";
+                    button_tile.classList.add("showed");
+                }
             }
         }
     }
@@ -54,7 +80,7 @@ class Board {
         button_tile.addEventListener("click", () => {
             this.showPossibleMoves(id);
         });
-        button_tile.innerHTML = '<img src = "static/img/empty.png" width=100%>';
+        button_tile.innerHTML = `<img src = ${ChessPiece.getImage("empty", "tile")} width=100%>`;;
         return button_tile;
     }
 
@@ -91,21 +117,51 @@ class Board {
         return board_js;
     }
 
-    placePiece(piece) {
-        this.deletePossibleMoves();
+    placePiece(piece, new_position=null) {
         let id = piece.getId();
         let button_tile = document.getElementById(id);
         let position = this.idToPosition(id);
-        button_tile.innerHTML = `<img src = ${piece.image} width=100%>`;
-        this.board_js[position[0]][position[1]] = piece;
+
+        if(new_position == null) {
+            button_tile.innerHTML = `<img src = ${piece.image} width=100%>`;
+            this.board_js[position[0]][position[1]] = piece;
+        }
+        else {
+            this.deletePossibleMoves();
+            button_tile.innerHTML = `<img src = ${ChessPiece.getImage("empty", "tile")} width=100%>`;;
+            this.board_js[position[0]][position[1]] = ChessPiece.EMPTY_TILE;
+            let new_id = this.positionToId(new_position);
+            let new_button_tile = document.getElementById(new_id);
+            piece.setId(new_id);
+            new_button_tile.innerHTML = `<img src = ${piece.image} width=100%>`;
+            this.board_js[new_position[0]][new_position[1]] = piece;
+        }
     }
 
 }
 
+function deleteMenu(menu) {
+    rows = document.getElementById("row").value;
+    columns = document.getElementById("col").value;
+    menu.remove();
+}
+
+function showMenu() {
+    let menu = document.getElementById("menu");
+    menu.innerHTML = "Type the quantity of rows and columns: <br>Row: <input type='number' id='row'> <br>Col: <input type='number' id='col'>";
+    let button = document.createElement("button");
+    button.setAttribute("id", "collector");
+    button.addEventListener("click", () => {
+        deleteMenu(menu);
+    });
+    menu.appendChild(button);
+}
+
 documentReady(()=>{
-    let board = new Board(8, 8);
-    let white_king = new King(2);
-    let white_tower = new Tower(1);
-    board.placePiece(white_king);
-    board.placePiece(white_tower);
+    // let board = new Board(8, 8);
+    // let white_king = new King(2);
+    // let white_tower = new Tower(1);
+    // board.placePiece(white_king);
+    // board.placePiece(white_tower);
+    showMenu();
 });
